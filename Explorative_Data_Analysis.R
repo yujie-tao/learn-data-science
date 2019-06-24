c58975e6e28e7aa9a3f0f29bd855745424690090
yujie-tao <yujietao@outlook.com>
2019-06-23 21:55:26 -0700

    add plot demonstration materials

--- Explorative_Data_Analysis.R
+++ Explorative_Data_Analysis.R
@@ -45,6 +45,35 @@ with (airquality, {
 })
 
 
+# Base plotting demonstration
+x <- rnorm(100)
+hist(x)
+y <- rnorm(100)
+plot(x, y)
+par(mar = c(2, 2, 2, 2))
+plot(x, y)
+plot(x, y, pch = 20) # pch is plot symbols
+example(points) # give a lists of plot symbols
+title('Scatterplot')
+text(-2, -2, 'label')
+legend('topleft', legend = 'Data', pch = 20)
+abline(fit)
+abline(fit, lwd = 3) # lwd = 3 make the line thicker
+abline(fit, lwd = 3, col = 'blue')
+plot(x, y, xlab = 'Weight', ylab = 'Height', main = 'Scatterplot', pch = 20)
+legend('topright', legend = 'Data', pch = 20)
+fit <- lm(y ~ x)
+abline(fit, lwd = 3, col= 'red')
+z <- rpois(200, 2)
+par(mfrow = c(2,1))
+plot(x, y , pch = 20)
+plot(x, y , pch = 10)
+par('mar')
+
+plot(x, y, type = 'n')
+points(x[g == 'Male'], y[g == 'Male'], col = 'green')
+points(x[g == 'Female'], y[g == 'Female'], col = 'blue')
+
 
 
 
