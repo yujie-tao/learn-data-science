7f7c757b4db795f73ae2b867f9b7e694c1d63a9a
yujie-tao <yujietao@outlook.com>
2019-07-08 01:09:04 -0700

    initialize reproducible research script

--- Explorative_Data_Analysis.R
+++ Explorative_Data_Analysis.R
@@ -153,8 +153,60 @@ set.seed(143)
 dataMatrix <- as.matrix(dataFrame)[sample(1:12),]
 heatmap(dataMatrix)
 
+# K-means clustering
+set.seed(1234)
+par(mar = c(0, 0, 0, 0))
+x <- rnorm (12, mean = rep(1:3, each = 4), sd = 0.2)
+plot(x, y, col ='blue', pch = 19, cex = 2)
+text(x+0.05, y+0.05, labels = as.character(1:12))
+
+dataFrame <- data.frame(x,y)
+kmeansObj <- kmeans(dataFrame, centers = 3)
+names(kmeansObj)
   
-  
-  
+kmeansObj$cluster
+par(mar = rep(0.2, 4))
+plot(x, y, col = kmeansObj$cluster, pch = 19, cex = 2)
+points(kmeansObj$center, col = 1:3, pch = 3, cex = 3, lwd = 3)
+
+set.seed(1234)
+dataMatrix <- as.matrix(dataFrame)[sample(1:12)]
+kmeansObjc2 <- kmeans(datamatrix, center = 3)
+par(mrow = c(1,2), mar = c(2, 4, 0.1, 0.1))
+image(t(dataMatrix[, nrow(dataMatrix):1], yaxt = 'n'))
+image(t(dataMatrix[, order(kmeansObj$cluster)], yaxt = 'n'))
+
+# Dimension reduction
+# principal components analysis and singular value decomposition
+set.seed(12345)
+par(mar = rep(0.2, 4))
+dataMatrix <- matrix(rnorm(400), nrow = 40)
+image(1:10, 1:40, t(dataMatrix)[, nrow(dataMatrix):1])
+pr(mar = rep(0.2, 4))
+heatmaap(dataMatrix)
+
+# patterns in rows and columns
+
+# components of SVD
+svd1 <- svd(scale(dataMatrixOrdered))
+par(mrow = c(1,3))
+image(t(dataMatrixOrdered)[,nrow(dataMatrixOrdered):1])
+plot(svd$u[,1], 40:1,xlab = 'Row', ylab = 'First left singular vector', pch = 19)
+plot(svd$v[,1], xlab = 'Column', ylab='First right singular vector', pch = 19)
+
+par(mfrow = c(1,2))
+plot(svd1$d, xlab = 'Column', ylab = 'Singular value', pch = 19)
+plot(svd$d^2/sum(svd$d^2), xlab = 'Column', ylab = 'Prop. of variance explained', pch = 19)
+abline(c(0,1))
+
+# pattenrs of variance in rows
+svd2 <- svd(scale(dataMatrixOrdered))
+par(mfrow = c(1,3))
+image(t(dataMatirxOrdered)[, nrow(datamatrixOrdered): 1])
+plot(svd2$v[ ,2], pch = 19, xlab = 'Column', ylab = 'First right singular vector')
+plot(svd$v[, 2], pch = 19, xlab = 'Column', ylab = 'Seconde right singular vector')
+
+
+
 
 
---
+++ Reproducible_Research.R
@@ -0,0 +1,2 @@
+# Reproducible Research
+# Be able to communicate and reproduce a data analysis
