8ad6fa439d49620110f7cf71a9f9f0453de3db21
yujie-tao <yujietao@outlook.com>
2019-07-15 23:58:39 -0700

    add statistical inference material

--- Statistical_Inference.R
+++ Statistical_Inference.R
@@ -1,1 +1,17 @@
 # Statistical Inference
+# Generating conclusions about a population from a noisy sample
+
+# Probability
+  # - Probability
+  # - Probability density function
+
+# Conditional Probability
+  # - Conditional probability
+  # - Bayes rule
+  # - Independence
+
+# Expected values
+  # - Mean is a charaterization of the center
+  # - The variance and standard deviation are how spread out it is
+  
+  
