9f765b7168fb1f1b7a87415cbae6c46159843970
yujie-tao <yujietao@outlook.com>
2019-07-19 20:12:51 -0700

    add LLN, CLT and confidence interval materials

--- Statistical_Inference.R
+++ Statistical_Inference.R
@@ -23,5 +23,27 @@
   # - Normal distribution
   pnorm(1160, mean = 1020, sd = 5, lower.tail = FALSE)
   pnorm(2.8, lower.tail = FALSE)
-  
+
   # - Poisson 
+  
+# Asymptotics and LLN (Law of Large Numbers)
+  n <- 1000
+  mean <- cumsum(rnorm(n))/(1:n)
+  means <- cumsum(sample(0:1, n, replace = TRUE))/(1:n)
+  # the mean converges
+  # LLN states that the average of iid samples converge to the population means they are estimating
+
+# Asymptotics and the CLT (Central Limit Theorem)
+   # CLT states that averages are approximately normal, with distribution 
+     # - centered at population mean
+     # - with standard deviation equal to the standard error of the mean
+     # - CLT gives no guarntee that n is large enough
+  
+# Asymptotics and confidence intervals
+   # Taking the mean ad adding and substracting the relevant normal quantiles times the SE yields a confidence interval for the mean
+     # - Adding and substracting 2 SEs works for 95% intervals
+  
+  
+  
+  
+  
