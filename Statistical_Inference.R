6440c4f1249e35a85a4c49932216bf4199fd8d4d
yujie-tao <yujietao@outlook.com>
2019-07-21 22:34:50 -0700

    add statistical inference and P value material

--- Statistical_Inference.R
+++ Statistical_Inference.R
@@ -44,8 +44,30 @@
      # - Adding and substracting 2 SEs works for 95% intervals
   
 
-# T confidence intervals
+# T Confidence Intervals
   # The t interval technically assumes that the data are iid normal, though it is robust to this assumption
   # Use T index rather than Z index, wider than normal, the more data, the more closer T distribution ton normal distribution
+  
+# Hypotheosis Testing
+  # Hypothesis testing is concerned with making decision using data
+  # A null hypothesis is specified that represents the status quo, usually labeled as H0. The null hypothesis is assumed true.
+  # Statistical evidence is required to reject H0 in favor of a research or alternative hypothesis, Ha
+  # Type 1 error: truth H0, decide Ha
+  # Type 2 error: truth Ha, decide H0
+  
+  # T Test
+    library(UsingR); data(fater.son)
+    t.test(fater.son$sheight - fater.son$fheight)
+  # Two sided test
+    # Suppose that we would reject the null hypothesis if in fact the mean is too large or too small
+  
+  # Two Group Testing
+
+# P values
+  # Most common measure of statistical significance
+  # The P-value is the probability under the null hypothesis of obtaining evidence as extreme or more extreme than that obtained
+    # If P-value is small. then either H0 is true, and we observed a rare event or Ha is false
+    # P indicates that, given a normal rate, wheteher the existed rate, is extreme enough. When p is small, it means that the existing rate is rare and H0 is true.
+    # E.g. Assume under 100 patients, 10 people get infected and if over 10 people get infected, we implement some regulation in response. But we also don't want to implement response by random number, like 20, we want to make sure 20 is truly bad infection sitution based on the scenario. We can calculate the P value here.
   
   
