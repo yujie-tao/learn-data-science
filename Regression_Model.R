399b1ae084dfd888ab56b5b8fe314585cdb7d87d
yujie-tao <yujietao@outlook.com>
2019-08-03 21:56:28 -0700

    add multivariable regression material

--- Regression_Model.R
+++ Regression_Model.R
@@ -28,3 +28,15 @@ n <- length(y)
 fit <- resid(fit)
 yhat <- predict(fit)
 max(abs(e- (y-yhat)))
+
+# Inference on regression
+
+# Multivariable regression analysis
+n = 100; x = rnorm(n); x2 = rnorm(x); x3 = rnorm(n)
+y = 1 + x + x2 + x3 + rnorm(n, sd = 0.1)
+ey = resid(lm(y~x2 + x3))
+ex = resi(lm(x~x2 + x3))
+sum(ey * ex)/sum(ex^2)
+coef(lm(y ~ x + x2 + x3))
+
+
