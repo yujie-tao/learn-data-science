0333416f531dad0516ad9360d7be8ea78ee64efd
yujie-tao <yujietao@outlook.com>
2019-08-20 00:20:07 -0400

    add hodgepodge material

--- Regression_Model.R
+++ Regression_Model.R
@@ -89,3 +89,13 @@ exp(logRegRaven$coeff)
 exp(confint(logregRavens))
 anova(logRegRavens, test = 'Chisq')
 
+# Hodgepodge
+n <- 500; x <- seq(0, 4*pi, length = n); y <- sin(x) + rnorm(n, sd=0.3)
+knots <- seq(0, 8*pi, length = 20)
+splineTerms <- sapply(knots, function(knot)(x > knot)*(x-knot))
+xMat <- cbind(1, x, spineTerms)
+yhat <- predict(lm(y ~ xMat - 1))
+plot(x, y, frame = FALSE, pch = 21, bg = 'lightblue', cex = 2)
+lines(x, yhat, col = 'red', lwd = 2)
+
+
