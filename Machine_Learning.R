76a2f2e61b3649736dce4eccacaf8bdfbea01e02
yujie-tao <yujietao@outlook.com>
2019-08-22 21:51:06 -0400

    add cross validation material

--- Machine_Learning.R
+++ Machine_Learning.R
@@ -5,3 +5,23 @@ lines(density(spam$Your[spam$type=='spam']),col='red')
 
 # Relative importance of steps
 # Gabage in = Gabage out
+
+# In and out sample errors
+# In Sample Error: The error rate you get on the same dataset you used to build your predictor. Sometimes called resubstitution error.
+# Out of Sample Error: The error rate you get on new data set. Sometimes called generalization error.
+
+# Types of errors
+
+# Basic terms
+# True positive = correctly identified
+# False positive = incorrectly identified
+# True negative = correctly rejected
+# False negative = incorrectly rejected
+
+# Snsitivity (recall): If you want few missed positive
+# Specifity: If you want few negatives called positives
+# Accuracy: Weights false positives/negatives equally
+
+# Cross validation
+# K-fold, larger k - less bias, more variance; smaller k - more bias ,less variance
+# Leave one out 
